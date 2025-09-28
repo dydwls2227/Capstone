ae4a5c7091266d78677c7f7da54c8b0725208fc0
dydwls2227 <dydwls2227@naver.com>
2025-09-29 04:36:26 +0900

    AccessToeken 구현

--- src/main/java/com/example/oauth2prac/entity/IndexController.java
+++ src/main/java/com/example/oauth2prac/entity/IndexController.java
@@ -1,58 +1,49 @@
-package com.example.oauth2prac.entity;
+    package com.example.oauth2prac.entity;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.security.core.annotation.AuthenticationPrincipal;
+    import org.springframework.security.core.context.SecurityContextHolder;
+    import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+    import org.springframework.security.oauth2.core.user.OAuth2User;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Map;
+    import java.util.Map;
 
-@Controller
-@RequiredArgsConstructor
-public class IndexController {
+    @Controller
+    @RequiredArgsConstructor
+    public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+        private final UserRepository userRepository;
+        private final JwtTokenProvider jwtTokenProvider;
 
-    @GetMapping("/success")
-    public String success(Model model, @AuthenticationPrincipal OAuth2User oAuth2User) {
-        if (oAuth2User == null) {
-            model.addAttribute("errorMessage", "로그인 정보가 없습니다.");
+        @GetMapping("/")
+        public String index() {
+            return "index";
+        }
+
+        @GetMapping("/success")
+        public String success(Model model, @AuthenticationPrincipal OAuth2User oAuth2User) {
+            if (oAuth2User == null) {
+                model.addAttribute("errorMessage", "로그인 정보가 없습니다.");
+                return "error";
+            }
+
+            String registerationId = ((OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication()).getAuthorizedClientRegistrationId();
+            OAuthAttributes oAuthAttributes = OAuthAttributes.of(registerationId, "sub", oAuth2User.getAttributes());
+
+            model.addAttribute("name", oAuthAttributes.getName());
+            model.addAttribute("email", oAuthAttributes.getEmail());
+
+            return "success";
+        }
+
+        @GetMapping("/error")
+        public String error(Model model) {
+            if (!model.containsAttribute("errorMessage")) {
+                model.addAttribute("errorMessage", "알 수 없는 오류가 발생했습니다.");
+            }
             return "error";
         }
-
-        String name = "";
-        String email = "";
-
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-
-        if (attributes.containsKey("sub")) {
-            // Google 로그인 처리
-            name = (String) attributes.get("name");
-            email = (String) attributes.get("email");
-        } else if (attributes.containsKey("id") && attributes.containsKey("kakao_account")) {
-            // Kakao 로그인 처리
-            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
-            name = (String) profile.get("nickname");
-            email = (String) kakaoAccount.get("email");
-        }
-
-        model.addAttribute("name", name);
-        model.addAttribute("email", email);
-
-        return "success";
     }
-
-    @GetMapping("/error")
-    public String error(Model model) {
-        if (!model.containsAttribute("errorMessage")) {
-            model.addAttribute("errorMessage", "알 수 없는 오류가 발생했습니다.");
-        }
-        return "error";
-    }
-}
---
+++ src/main/java/com/example/oauth2prac/entity/JwtTokenProvider.java
@@ -0,0 +1,56 @@
+package com.example.oauth2prac.entity;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private static final long ACCESS_TOKEN_VALIDITY = 1000L*60*60;
+    private static final long REFRESH_TOKEN_VALIDITY = 1000*60*60*24*14;
+
+    private final Key key = Keys.hmacShaKeyFor("01234567890123456789012345678901".getBytes(StandardCharsets.UTF_8));
+
+
+    public String createToken(String userId, String role) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY))
+                .signWith(key, io.jsonwebtoken.SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_VALIDITY))
+                .signWith(key,io.jsonwebtoken.SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getUserId(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
