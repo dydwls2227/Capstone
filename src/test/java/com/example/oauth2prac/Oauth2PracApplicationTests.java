648244955d7fd4ca1bcdaf411460059f6db19023
dydwls2227 <dydwls2227@naver.com>
2025-10-24 18:24:22 +0900

    테스트코드

--- src/test/java/com/example/oauth2prac/Oauth2PracApplicationTests.java
+++ src/test/java/com/example/oauth2prac/Oauth2PracApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.oauth2prac;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("oauth") // 테스트 실행 시 "oauth" 프로필을 활성화합니다.
 @SpringBootTest
 class Oauth2PracApplicationTests {
 
